9264bdbf6a4e033b0c409f8e6715bcb7ad92a4a5
jeffrho68 <jeffrho68@gmail.com>
2013-08-31 11:23:48 -0500

    Added empty test case

---
+++ TestMVC/src/test/java/org/jeffrho/testmvc/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package org.jeffrho.testmvc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
